1e5e7efa480f35fcb112aea66a078d0d118798d6
manuc66 <manuc66@gmail.com>
2023-08-13 16:33:53 +0200

    Add atg, asign, unasign

--- ImgTagFanOut/ViewModels/MainWindowViewModel.cs
+++ ImgTagFanOut/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Reactive;
-using System.Reactive.Joins;
 using System.Reactive.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,6 +18,147 @@ using ReactiveUI;
 
 namespace ImgTagFanOut.ViewModels;
 
+public class Tag : IEquatable<Tag>
+{
+    public string Name { get; }
+
+    public Tag(string name)
+    {
+        Name = name;
+    }
+
+    public bool Equals(Tag? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return string.Equals(Name, other.Name, StringComparison.OrdinalIgnoreCase);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Tag)obj);
+    }
+
+    private sealed class NameEqualityComparer : IEqualityComparer<Tag>
+    {
+        public bool Equals(Tag x, Tag y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (ReferenceEquals(x, null)) return false;
+            if (ReferenceEquals(y, null)) return false;
+            if (x.GetType() != y.GetType()) return false;
+            return x.Equals(y);
+        }
+
+        public int GetHashCode(Tag obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+
+    public static IEqualityComparer<Tag> Comparer { get; } = new NameEqualityComparer();
+
+    public override int GetHashCode()
+    {
+        return StringComparer.OrdinalIgnoreCase.GetHashCode(Name);
+    }
+}
+
+public class CanHaveTag<T> : ViewModelBase
+{
+    private T _item;
+    private ObservableCollection<Tag> _tags = new();
+
+    public ObservableCollection<Tag> Tags
+    {
+        get => _tags;
+        set => this.RaiseAndSetIfChanged(ref _tags, value);
+    }
+
+    public CanHaveTag(T item)
+    {
+        this._item = item;
+    }
+
+    public void AddTag(Tag tag)
+    {
+        if (!_tags.Any(x => x.Equals(tag)))
+        {
+            _tags.Add(tag);
+        }
+    }
+
+    public void RemoveTag(Tag tag)
+    {
+        if (_tags.Any(x => x.Equals(tag)))
+        {
+            _tags.Remove(tag);
+        }
+    }
+
+    public void Toggle(Tag tag)
+    {
+        if (!_tags.Any(x => x.Equals(tag)))
+        {
+            _tags.Add(tag);
+        }
+        else
+        {
+            _tags.Remove(tag);
+        }
+    }
+}
+
+class TagRepository
+{
+    private readonly HashSet<Tag> _tags = new HashSet<Tag>(Tag.Comparer);
+
+    public bool TryCreateTag(string? tagName, [MaybeNullWhen(false)] out Tag newTag)
+    {
+        if (!string.IsNullOrWhiteSpace(tagName?.Trim()))
+        {
+            newTag = new Tag(tagName.Trim());
+            _tags.Add(newTag);
+            return true;
+        }
+
+        newTag = null;
+        return false;
+    }
+
+    public ImmutableList<Tag> GetAll()
+    {
+        return _tags.ToImmutableList();
+    }
+
+    public void AddTagToItem<T>(string tagName, CanHaveTag<T> tagAssignation)
+    {
+        if (_tags.TryGetValue(new Tag(tagName), out Tag? existingTag))
+        {
+            tagAssignation.AddTag(existingTag);
+        }
+    }
+
+    public void RemoveTagToItem<T>(string tagName, CanHaveTag<T> tagAssignation)
+    {
+        if (_tags.TryGetValue(new Tag(tagName), out Tag? existingTag))
+        {
+            tagAssignation.RemoveTag(existingTag);
+        }
+    }
+
+    public void ToggleToItem(string tagName, CanHaveTag<string> tagAssignation)
+    {
+        if (_tags.TryGetValue(new Tag(tagName), out Tag? existingTag))
+        {
+            tagAssignation.Toggle(existingTag);
+        }
+    }
+}
+
 public class MainWindowViewModel : ViewModelBase
 {
     private string _workingFolder = string.Empty;
@@ -27,6 +168,8 @@ public class MainWindowViewModel : ViewModelBase
     private string? _tagFilterInput;
     private ObservableCollection<string> _tagList = new();
     private List<string> _filteredTagList;
+    private readonly TagRepository _tagRepository = new();
+    private CanHaveTag<string>? _selectedImageTag;
 
     public string WorkingFolder
     {
@@ -39,6 +182,10 @@ public class MainWindowViewModel : ViewModelBase
     public ReactiveCommand<Unit, Unit> ScanFolderCommand { get; }
 
     public ReactiveCommand<Unit, Unit> CancelScanCommand { get; }
+
+    public ReactiveCommand<String, Unit> ToggleAssignTagToImageCommand { get; }
+
+    public ReactiveCommand<Tag, Unit> RemoveTagToImageCommand { get; }
 
     public ObservableCollection<string> Images
     {
@@ -64,6 +211,12 @@ public class MainWindowViewModel : ViewModelBase
         set => this.RaiseAndSetIfChanged(ref _selectedImage, value);
     }
 
+    public CanHaveTag<string>? SelectedImageTag
+    {
+        get => _selectedImageTag;
+        set => this.RaiseAndSetIfChanged(ref _selectedImageTag, value);
+    }
+
     public Bitmap? ImageToDisplay
     {
         get => _imageToDisplay;
@@ -82,14 +235,18 @@ public class MainWindowViewModel : ViewModelBase
 
     public MainWindowViewModel()
     {
-        TagList = new ObservableCollection<string>() { "Luca", "Louis", "Filip" };
+        _tagRepository.TryCreateTag("Lucas", out _);
+        _tagRepository.TryCreateTag("Louis", out _);
+        _tagRepository.TryCreateTag("Filip", out _);
+        TagList = new ObservableCollection<string>(_tagRepository.GetAll().Select(x => x.Name));
+        _filteredTagList = new List<string>();
 
         ScanFolderCommand = ReactiveCommand.CreateFromObservable(
             () => Observable
                 .StartAsync(ScanFolder)
                 .TakeUntil(CancelScanCommand!));
         ScanFolderCommand.Subscribe(x => { });
-        SelectFolderCommand = ReactiveCommand.CreateFromTask<Window, string>(async (Window window) =>
+        SelectFolderCommand = ReactiveCommand.CreateFromTask<Window, string>(async window =>
         {
             FolderPickerOpenOptions folderPickerOptions = new()
             {
@@ -119,9 +276,9 @@ public class MainWindowViewModel : ViewModelBase
 
         AddToTagListCommand = ReactiveCommand.Create(() =>
         {
-            if (!string.IsNullOrWhiteSpace(TagFilterInput))
+            if (_tagRepository.TryCreateTag(TagFilterInput, out Tag? newTag))
             {
-                TagList.Add(TagFilterInput.Trim());
+                TagList.Add(newTag.Name);
             }
         }, this.WhenAnyValue(x => x.TagFilterInput)
             .Select(x => !string.IsNullOrWhiteSpace(x) && !TagList.Any(tag => tag.Equals(x.Trim(), StringComparison.OrdinalIgnoreCase)))
@@ -131,6 +288,18 @@ public class MainWindowViewModel : ViewModelBase
                 .Select(collection => !collection.Any(tag => tag.Equals(TagFilterInput, StringComparison.OrdinalIgnoreCase))), (b, b1) => b && b1));
         ClearTagFilterInputCommand = ReactiveCommand.Create(() => { TagFilterInput = String.Empty; },
             this.WhenAnyValue(x => x.TagFilterInput).Select(x => !string.IsNullOrWhiteSpace(x)));
+
+        ToggleAssignTagToImageCommand = ReactiveCommand.Create(
+            (String s) =>
+            {
+                if (SelectedImageTag != null)
+                {
+                    _tagRepository.ToggleToItem(s, SelectedImageTag);
+                }
+            });
+
+        RemoveTagToImageCommand = ReactiveCommand.Create(
+            (Tag s) => { _tagRepository.RemoveTagToItem(s.Name, SelectedImageTag); });
 
         this.WhenAnyValue(x => x.TagFilterInput).Subscribe(tagFilterInput =>
         {
@@ -171,6 +340,8 @@ public class MainWindowViewModel : ViewModelBase
                 {
                     ImageToDisplay = new Bitmap(fs);
                 }
+
+                SelectedImageTag = new CanHaveTag<string>(fullFilePath);
 
                 previous?.Dispose();
             });
