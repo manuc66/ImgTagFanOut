1f11fbc8213752e2c8c80f0eff82a5072b1a3508
manuc66 <manuc66@gmail.com>
2023-09-12 21:24:49 +0200

    Try to add Sentry without SDK, see : https://github.com/getsentry/sentry-dotnet/issues/2614

--- ImgTagFanOut/Program.cs
+++ ImgTagFanOut/Program.cs
@@ -1,9 +1,9 @@
 ﻿using Avalonia;
 using Avalonia.ReactiveUI;
 using System;
-using System.IO;
-using System.Reflection;
-using Avalonia.Dialogs;
+using System.Reactive;
+using ReactiveUI;
+using Sentry;
 
 namespace ImgTagFanOut;
 
@@ -23,6 +23,34 @@ class Program
             .With(new X11PlatformOptions
             {
                 UseDBusFilePicker = false // to disable FreeDesktop file picker
+            })
+            .AfterSetup(_ =>
+            {
+                SentrySdk.Init(o =>
+                {
+                    // Tells which project in Sentry to send events to:
+                    o.Dsn = "https://2fd61307fdf9b3a63804db34c9bc51eb@o4505868956860416.ingest.sentry.io/4505869112639488";
+                    // When configuring for the first time, to see what the SDK is doing:
+                    o.Debug = true;
+                    
+                    // This option is recommended. It enables Sentry's "Release Health" feature.
+                    o.AutoSessionTracking = true;
+                    
+                    // Set TracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+                    // We recommend adjusting this value in production.
+                    o.TracesSampleRate = 1.0;
+                });
+                AppDomain.CurrentDomain.UnhandledException += (object sender, UnhandledExceptionEventArgs e) =>
+                {
+                    if (e.ExceptionObject is Exception ex)
+                    {
+                        SentrySdk.CaptureException(ex);
+                    }
+                };
+                RxApp.DefaultExceptionHandler = Observer.Create<Exception>(e =>
+                {
+                    SentrySdk.CaptureException(e);
+                });
             })
             .WithInterFont()
             .LogToTrace()
