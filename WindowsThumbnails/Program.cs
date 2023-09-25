14e332f183d84011c27de43132bccadacbc17181
Emmanuel Counasse <manuc66@gmail.com>
2023-09-25 21:10:34 +0200

    Add idea to have cache from windows thumbnail

---
+++ WindowsThumbnails/Program.cs
@@ -0,0 +1,23 @@
+﻿using DisplayMagician;
+
+namespace WindowsThumbnails
+{
+
+    public class Program
+    {
+
+
+
+        public static void Main()
+        {
+            var enumerateFiles = Directory.EnumerateFiles(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "*.*", SearchOption.AllDirectories);
+            foreach (var file in enumerateFiles)
+            {
+                Console.Write(file);
+                var thumbnail = WindowsThumbnailProvider.GetThumbnail(file, 256, 256, ThumbnailOptions.WideThumbnails);
+                Console.WriteLine(" .");
+                thumbnail.Dispose();
+            }
+        }
+    }
+}
