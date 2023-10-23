15631dd93231c9eab9e6cf08c8f21f21b1b231f5
Emmanuel Counasse <manuc66@gmail.com>
2023-10-23 20:04:33 +0200

    Extract view AssignTags

---
+++ ImgTagFanOut/Views/AssignTags.axaml.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using ImgTagFanOut.ViewModels;
+
+namespace ImgTagFanOut.Views;
+
+public partial class AssignTags : ReactiveUserControl<MainWindowViewModel>
+{
+    public AssignTags()
+    {
+        InitializeComponent();
+    }
+}
