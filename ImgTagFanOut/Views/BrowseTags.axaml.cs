0a36f5daa69e9119410d0f898d47dbf984148fbc
Emmanuel Counasse <manuc66@gmail.com>
2023-10-24 21:06:21 +0200

    Extract BrowseTags user control

---
+++ ImgTagFanOut/Views/BrowseTags.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ImgTagFanOut.Views;
+
+public partial class BrowseTags : UserControl
+{
+    public BrowseTags()
+    {
+        InitializeComponent();
+    }
+}
