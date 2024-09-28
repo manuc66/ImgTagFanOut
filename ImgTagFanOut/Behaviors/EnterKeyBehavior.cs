0a738c6a14c71ac22d6cc7d4f3184cc31204a21f
Emmanuel Counasse <emmanuel.counasse@gaming1.com>
2024-09-28 23:16:46 +0200

    Add tag with Enter key

---
+++ ImgTagFanOut/Behaviors/EnterKeyBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Xaml.Interactivity;
+
+namespace ImgTagFanOut.Behaviors;
+
+public class EnterKeyBehavior : Behavior<TextBox>
+{
+    public static readonly StyledProperty<ICommand> CommandProperty =
+        AvaloniaProperty.Register<EnterKeyBehavior, ICommand>(nameof(Command));
+
+    public ICommand Command
+    {
+        get => GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        if (AssociatedObject != null) AssociatedObject.KeyDown += OnKeyDown;
+    }
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+        if (AssociatedObject != null) AssociatedObject.KeyDown -= OnKeyDown;
+    }
+
+    private void OnKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter && Command.CanExecute(null))
+        {
+            Command.Execute(null);
+        }
+    }
+}
