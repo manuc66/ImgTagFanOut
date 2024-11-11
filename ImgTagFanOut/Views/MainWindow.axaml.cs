870c1a3c60b7ca27ccd81ef09d6883f9047c7ae3
manuc66 <manuc66@gmail.com>
2024-11-11 19:32:05 +0100

    Update dependency

--- ImgTagFanOut/Views/MainWindow.axaml.cs
+++ ImgTagFanOut/Views/MainWindow.axaml.cs
@@ -15,12 +15,11 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
     public MainWindow()
     {
         InitializeComponent();
-        
         this.WhenActivated(d => d(ViewModel!.PublishDropOrMergeDialog.RegisterHandler(DoShowPublishDropOrMergeDialogAsync)));
         this.WhenActivated(d => d(ViewModel!.ShowPublishProgressDialog.RegisterHandler(DoShowPublishProgressDialogAsync)));
         this.WhenActivated(d => d(ViewModel!.ShowConsentDialog.RegisterHandler(DoShowConsentDialogAsync)));
         this.WhenActivated(d => d(ViewModel!.ShowAboutDialog.RegisterHandler(DoShowABoutDialogAsync)));
-        this.WhenActivated(d => d(ViewModel!.ExitCommand.Subscribe(x => Close())));
+        this.WhenActivated(d => d(ViewModel!.ExitCommand.Subscribe(_ => Close())));
         Activated += OnWindowActivated;
         this.WhenActivated(disposable =>
         {
@@ -30,6 +29,7 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
                 .DisposeWith(disposable);
         });
     }
+
     private void UpdateCursor(bool isBusy)
     {
         if (isBusy)
@@ -47,7 +47,7 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
         Dispatcher.UIThread.Post(() => ViewModel!.WindowActivated = true);
     }
 
-    private async Task DoShowPublishDropOrMergeDialogAsync(InteractionContext<PublishDropOrMergeViewModel, int?> interaction)
+    private async Task DoShowPublishDropOrMergeDialogAsync(IInteractionContext<PublishDropOrMergeViewModel, int?> interaction)
     {
         PublishDropOrMergeWindow dialog = new()
         {
@@ -57,7 +57,8 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
         int? result = await dialog.ShowDialog<int?>(this);
         interaction.SetOutput(result);
     }
-    private async Task DoShowPublishProgressDialogAsync(InteractionContext<PublishProgressViewModel, int?> interaction)
+
+    private async Task DoShowPublishProgressDialogAsync(IInteractionContext<PublishProgressViewModel, int?> interaction)
     {
         PublishProgressWindow dialog = new()
         {
@@ -67,7 +68,8 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
         int? result = await dialog.ShowDialog<int?>(this);
         interaction.SetOutput(result);
     }
-    private async Task DoShowConsentDialogAsync(InteractionContext<ConsentViewModel, int?> interaction)
+
+    private async Task DoShowConsentDialogAsync(IInteractionContext<ConsentViewModel, int?> interaction)
     {
         ConsentWindow dialog = new()
         {
@@ -77,7 +79,8 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
         int? result = await dialog.ShowDialog<int?>(this);
         interaction.SetOutput(result);
     }
-    private async Task DoShowABoutDialogAsync(InteractionContext<AboutViewModel, int?> interaction)
+
+    private async Task DoShowABoutDialogAsync(IInteractionContext<AboutViewModel, int?> interaction)
     {
         About dialog = new()
         {
